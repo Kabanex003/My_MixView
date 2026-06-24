39a0954f37fd17fb4ea8594adc6aa191f5200ab6
Kabanex03 <aon2546.a@gmail.com>
2026-06-24 21:16:36 +0700

    first commit2

---
+++ android/app/src/main/kotlin/com/example/mixview/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mixview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/mixview.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MyMixViewPage extends StatefulWidget {
+  const MyMixViewPage({super.key});
+
+  @override
+  State<MyMixViewPage> createState() => _MyMixViewPageState();
+}
+
+class _MyMixViewPageState extends State<MyMixViewPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
